50641889ae56b59cfcdc6f51d495286cdbe3538c
dmaish <danieldeveloperemail@gmail.com>
2025-08-01 19:35:58 +0300

    add env vars

--- services/axios.js
+++ services/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
-const API_BASE_URL = 'http://localhost:5000/api';  
+const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
